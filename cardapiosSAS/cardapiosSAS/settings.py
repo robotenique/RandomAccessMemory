f03aeeadec7fc83cf1c56332feba859d58fc0d5f
Juliano Garcia <julianogarcia_1997@hotmail.com>
2016-10-28 23:45:24 -0200

    Add url processing through scrapy-splash

--- cardapiosSAS/cardapiosSAS/settings.py
+++ cardapiosSAS/cardapiosSAS/settings.py
@@ -14,15 +14,23 @@ BOT_NAME = 'cardapiosSAS'
 SPIDER_MODULES = ['cardapiosSAS.spiders']
 NEWSPIDER_MODULE = 'cardapiosSAS.spiders'
 
-SPLASH_URL = 'http://192.168.59.103:8050'
-DOWNLOADER_MIDDLEWARES = {
-    'scrapy_splash.SplashCookiesMiddleware': 723,
-    'scrapy_splash.SplashMiddleware': 725,
-    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
-}
-SPIDER_MIDDLEWARES = {
-    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
-}
+# scrapy_splash CONFIGS!
+#SPLASH_URL = 'http://192.168.59.103:8050'
+#DOWNLOADER_MIDDLEWARES = {
+#    'scrapy_splash.SplashCookiesMiddleware': 723,
+#    'scrapy_splash.SplashMiddleware': 725,
+#    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+#}
+#SPIDER_MIDDLEWARES = {
+#    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+#}
+#
+##Override Scrapy fingerprint algorithms
+#DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+#HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+
+###############################################################################
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'cardapiosSAS (+http://www.yourdomain.com)'
 
--- cardapiosSAS/cardapiosSAS/spiders/cardapios_spider.py
+++ cardapiosSAS/cardapiosSAS/spiders/cardapios_spider.py
@@ -1,23 +1,22 @@
 import scrapy
 
-
 class CardapioSpider(scrapy.Spider):
     # Spider Identifier
     name = "cardapiosSAS"
+    allowed_domains = ["https://uspdigital.usp.br/"]
+    start_urls = ['http://localhost:8050/render.html?url=https://uspdigital.usp.br/rucard/Jsp/cardapioSAS.jsp?codrtn=8&timeout=10&wait=0.5']
 
     def start_requests(self):
-        # The urls to parse
-        urls = ["https://uspdigital.usp.br/rucard/Jsp/cardapioSAS.jsp?codrtn=8"]
-        # Get the url and send it into the callback
-        for url in urls:
-            yield scrapy.Request(url = url, callback=self.parse)
+        # Pass request through splash
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback = self.parse)
 
     # Process our response to the desired format
     def parse(self, response):
         # Symbol Table with key - value
         dictRest = {'8':'Física', '9':'Química'}
         # Modify this parse method
-        page = dictRest[response.url.split("/")[-1][-1]]
+        page = dictRest[response.url.split("/")[-1][-21]]
         filename = 'c_%s.html' % page
         with open(filename, mode='wb') as f:
             f.write(response.body)
